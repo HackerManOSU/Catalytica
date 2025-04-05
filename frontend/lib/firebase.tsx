7d13f174d773500337569b40cc48b4f93ce7b47b
Reptop <raed.kabir03@gmail.com>
2025-04-05 13:57:43 -0700

    Added firebase config and dependancy

---
+++ frontend/lib/firebase.tsx
@@ -0,0 +1,26 @@
+// Firebase SDKs
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your Firebase project config
+const firebaseConfig = {
+  apiKey: "input secret here or whatever the name is",
+  authDomain: "catalytica-b8ad9.firebaseapp.com",
+  projectId: "catalytica-b8ad9",
+  storageBucket: "catalytica-b8ad9.firebasestorage.app",
+  messagingSenderId: "891546386409",
+  appId: "1:891546386409:web:1af68f77fb0596a6511578",
+  measurementId: "G-Q2DB40DMKE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { app, analytics, db, auth, provider };
+
