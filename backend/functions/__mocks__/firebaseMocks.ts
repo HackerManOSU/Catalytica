b0a896782e8a1198994ef32178dad4715caa5944
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-04-06 00:46:42 -0700

    rm backend

--- backend/functions/__mocks__/firebaseMocks.ts
+++
@@ -1,8 +0,0 @@
-import { jest } from '@jest/globals';
-
-export const db = {
-    collection: jest.fn().mockReturnThis(),
-    add: jest.fn(),
-    get: jest.fn(),
-    doc: jest.fn(),
-  };
--- backend/functions/src/models/weather.ts
+++
@@ -1,6 +0,0 @@
-export interface Weather {
-    weather_desc: string;
-    moisture: number;
-    wind_speed: number;
-    wind_direction: string;
-}
