221b0ec0127baf3237a197352cc042f181ec83b5
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-04-06 00:45:53 -0700

    rm lib

--- frontend/functions/lib/controllers/weatherControllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export async function deleteWeatherController(weatherId: String) {
-//     try {
-//         const result = await 
-//     }
-// }
-//# sourceMappingURL=weatherControllers.js.map
